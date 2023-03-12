6811ea1252f710d04ede63f06c6a154bcfb70066
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-03-12 17:56:07 +0200

    feat: add next task solution

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -19,9 +19,16 @@ Hello JS
 // var myFirstName = "Aliaksandr";
 // var myLastName = "Hapankou";
 
-var a = 5;
-var b = 10;
-var c = "I am a";
-a = a + 1;
-b = b + 5;
-c = c + " String!";
+// var a = 5;
+// var b = 10;
+// var c = "I am a";
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
