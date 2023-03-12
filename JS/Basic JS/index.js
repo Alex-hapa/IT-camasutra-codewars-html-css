15cbff9e48afb5f2661496fc4ec64b1da1384c63
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-03-12 18:27:46 +0200

    feat: add next task solution

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -36,7 +36,9 @@ Hello JS
 // let catName = "Oliver";
 // let catSound = "Meow!";
 
-const FCC = "freeCodeCamp";
-let fact = "is cool!";
-fact = "is awesome!";
-console.log(FCC, fact);
+// const FCC = "freeCodeCamp";
+// let fact = "is cool!";
+// fact = "is awesome!";
+// console.log(FCC, fact);
+
+const sum = 10 + 10;
