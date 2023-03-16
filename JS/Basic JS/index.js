a7dd6f0b66c6ebd759c4623ae4779f0f3953d35c
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-03-16 22:00:41 +0200

    feat: add next task solution

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -94,4 +94,6 @@ Hello JS
 
 // const myStr = 'I am a "double quoted" string inside "double quotes".';
 
-const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+// const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
