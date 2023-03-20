0b00bbcb153f40bf248870af40453939ef820638
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-03-20 22:17:05 +0200

    feat: add next task solution

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -98,4 +98,7 @@ Hello JS
 
 // const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
 
-const myStr = "This is the start." + " This is the end.";
+// const myStr = "This is the start." + " This is the end.";
+
+let myStr = "This is the first sentence.";
+myStr += " This is the second sentence.";
