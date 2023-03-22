0f4b8f73ea6605f7462ce0a82b678bda94b09e85
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-03-22 21:54:34 +0200

    feat: add next task solution

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -100,5 +100,12 @@ Hello JS
 
 // const myStr = "This is the start." + " This is the end.";
 
-let myStr = "This is the first sentence.";
-myStr += " This is the second sentence.";
+// let myStr = "This is the first sentence.";
+// myStr += " This is the second sentence.";
+
+// const myName = "Aliaksandr";
+// const myStr = "My name is " + myName + " and I am well!";
+
+const someAdjective = "amazing";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
