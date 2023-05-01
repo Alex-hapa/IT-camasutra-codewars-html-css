0b80631c7db0418c822a57b83fbc86ad9d1fe3bf
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-05-01 19:47:44 +0300

    feat: add next task solution codewars

---
+++ codewars/7kyuExesAndOhs.js
@@ -0,0 +1,18 @@
+function XO(str) {
+    let countX = 0;
+    let countO = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].toLowerCase() === 'x') {
+            countX++;
+        }
+        if (str[i].toLowerCase() === 'o') {
+            countO++;
+        }
+    }
+    if (countX === countO){
+        return true
+    } else {
+        return false
+    }
+    return true;
+}
