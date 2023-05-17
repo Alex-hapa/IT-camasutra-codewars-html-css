ced854cada87b8976331d61df4d1e57e9882efb1
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-05-17 17:13:28 +0300

    feat: add next solution codewars

---
+++ codewars/7kyuCountTheDivisorsOfaNumber.js
@@ -0,0 +1,12 @@
+function getDivisorsCnt(n) {
+    let count = 0;
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            count++;
+            if (i !== Math.sqrt(n)) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
