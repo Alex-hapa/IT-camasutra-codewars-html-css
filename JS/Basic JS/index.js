23ef39237571757cd1294abdb81bbccd914cca78
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-05-29 22:02:28 +0300

    feat: add next solution task

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -184,7 +184,13 @@ Hello JS
 //     return num * 5;
 // }
 
-const myGlobal = 10;
-function fun1() {
-    oopsGlobal = 5;
+// const myGlobal = 10;
+// function fun1() {
+//     oopsGlobal = 5;
+// }
+
+function myLocalScope() {
+    const myVar = 0;
+    console.log('inside myLocalScope', myVar);
 }
+myLocalScope();
