fd48e86a84a6cc2c67ccd1a0f5cc548241290529
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-06-25 18:40:53 +0300

    feat: add next solution task

---
+++ codewars/8kyuQuarteroftheyear.js
@@ -0,0 +1,12 @@
+const quarterOf = (month) => {
+    if (month >= 1 && month < 4)  {
+        return 1;
+    }
+    if (month >= 4 && month < 7)  {
+        return 2;
+    }
+    if (month >= 7 && month < 10)  {
+        return 3;
+    }
+    return 4;
+}
