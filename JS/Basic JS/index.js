4cbdd21d34a7b00acbf8c444f12ed17cc20da4a1
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-06-25 19:24:37 +0300

    feat: add next solution task

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -228,9 +228,18 @@ Hello JS
 //     return true;
 // }
 
-function trueOrFalse(wasThatTrue) {
-    if (wasThatTrue) {
-        return "Yes, that was true";
+// function trueOrFalse(wasThatTrue) {
+//     if (wasThatTrue) {
+//         return "Yes, that was true";
+//     }
+//     return "No, that was false";
+// }
+
+function testEqual(val) {
+    if (val == 12) {
+        return "Equal";
     }
-    return "No, that was false";
+    return "Not Equal";
 }
+
+testEqual(10);
