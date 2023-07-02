7f4bb9e84132fb724b5ed3f5c4c63e141b5aa158
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-07-02 11:43:02 +0300

    feat: add next solution task

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -346,14 +346,26 @@ Hello JS
 //
 // testLogicalOr(15);
 
-function testElse(val) {
-    let result = "";
-    if (val > 5) {
-        result = "Bigger than 5";
+// function testElse(val) {
+//     let result = "";
+//     if (val > 5) {
+//         result = "Bigger than 5";
+//     } else {
+//         result = "5 or Smaller";
+//     }
+//     return result;
+// }
+//
+// testElse(4);
+
+function testElseIf(val) {
+    if (val > 10) {
+        return "Greater than 10";
+    } else if (val < 5) {
+        return "Smaller than 5";
     } else {
-        result = "5 or Smaller";
+        return "Between 5 and 10";
     }
-    return result;
 }
 
-testElse(4);
+testElseIf(7);
