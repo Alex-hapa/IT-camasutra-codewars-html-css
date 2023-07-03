554b88695209b443899733cfad1254cb12c4ea03
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-07-03 20:04:16 +0300

    feat: add next solution task

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -441,23 +441,48 @@ Hello JS
 //
 // caseInSwitch(1);
 
-function switchOfStuff(val) {
+// function switchOfStuff(val) {
+//     let answer = "";
+//     switch (val) {
+//         case 'a' :
+//             answer = 'apple'
+//             break;
+//         case 'b' :
+//             answer = 'bird'
+//             break;
+//         case 'c' :
+//             answer = 'cat'
+//             break;
+//         default :
+//             answer = 'stuff'
+//             break;
+//     }
+//     return answer;
+// }
+//
+// switchOfStuff(1);
+
+function sequentialSizes(val) {
     let answer = "";
     switch (val) {
-        case 'a' :
-            answer = 'apple'
+        case 1 :
+        case 2 :
+        case 3 :
+            answer = 'Low'
             break;
-        case 'b' :
-            answer = 'bird'
+        case 4 :
+        case 5 :
+        case 6 :
+            answer = 'Mid'
             break;
-        case 'c' :
-            answer = 'cat'
+        case 7 :
+        case 8 :
+        case 9 :
+            answer = 'High'
             break;
-        default :
-            answer = 'stuff'
-            break;
+
     }
     return answer;
 }
 
-switchOfStuff(1);
+sequentialSizes(1);
