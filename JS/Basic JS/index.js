1c46604ed820dc6b87350f7e28cde9098a469d7e
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-07-04 22:51:20 +0300

    feat: add next solution task

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -517,11 +517,39 @@ Hello JS
 //
 // isLess(10, 15);
 
-function abTest(a, b) {
-    if (a < 0 || b <0) {
-        return undefined;
+// function abTest(a, b) {
+//     if (a < 0 || b <0) {
+//         return undefined;
+//     }
+//     return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+// }
+//
+// abTest(2,2);
+
+let count = 0;
+
+function cc(card) {
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--;
+            break;
     }
-    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+    if (count > 0) {
+        return count + " Bet";
+    } else {
+        return count + " Hold";
+    }
 }
 
-abTest(2,2);
+cc(2); cc(3); cc(7); cc('K'); cc('A');
