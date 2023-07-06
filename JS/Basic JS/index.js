d7e25b8bb5dfce7564ded32cbdf2008166ff6ecc
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-07-06 22:20:34 +0300

    feat: add next solution task

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -593,10 +593,19 @@ Hello JS
 // };
 // myDog.name = 'Happy Coder'
 
+// const myDog = {
+//     "name": "Happy Coder",
+//     "legs": 4,
+//     "tails": 1,
+//     "friends": ["freeCodeCamp Campers"]
+// };
+// myDog.bark = 'woof';
+
 const myDog = {
     "name": "Happy Coder",
     "legs": 4,
     "tails": 1,
-    "friends": ["freeCodeCamp Campers"]
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
 };
-myDog.bark = 'woof';
+delete myDog.tails;
