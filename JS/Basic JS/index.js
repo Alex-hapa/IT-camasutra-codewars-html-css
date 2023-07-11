fc2fa93d8c550314413c38d3b77c1dc7a6cf0d94
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-07-11 20:38:33 +0300

    feat: add next solution task

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -632,27 +632,40 @@ Hello JS
 //     return "Not Found";
 // }
 
-const myMusic = [
-    {
-        "artist": "Billy Joel",
-        "title": "Piano Man",
-        "release_year": 1973,
-        "formats": [
-            "CD",
-            "8T",
-            "LP"
-        ],
-        "gold": true
-    },
-    {
-        "artist": "Deep Purple",
-        "title": "Smoke on the water",
-        "release_year": 1976,
-        "formats": [
-            "CD",
-            "8T",
-            "LP"
-        ],
-        "gold": true
+// const myMusic = [
+//     {
+//         "artist": "Billy Joel",
+//         "title": "Piano Man",
+//         "release_year": 1973,
+//         "formats": [
+//             "CD",
+//             "8T",
+//             "LP"
+//         ],
+//         "gold": true
+//     },
+//     {
+//         "artist": "Deep Purple",
+//         "title": "Smoke on the water",
+//         "release_year": 1976,
+//         "formats": [
+//             "CD",
+//             "8T",
+//             "LP"
+//         ],
+//         "gold": true
+//     }
+// ];
+
+const myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
     }
-];
+};
+const gloveBoxContents = myStorage.car.inside['glove box'];
