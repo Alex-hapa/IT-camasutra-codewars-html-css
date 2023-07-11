906ecd26063bcdd4c072cd98e8c1339cdb3ce23c
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-07-11 21:08:46 +0300

    feat: add next solution task

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -670,22 +670,56 @@ Hello JS
 // };
 // const gloveBoxContents = myStorage.car.inside['glove box'];
 
-const myPlants = [
-    {
-        type: "flowers",
-        list: [
-            "rose",
-            "tulip",
-            "dandelion"
-        ]
+// const myPlants = [
+//     {
+//         type: "flowers",
+//         list: [
+//             "rose",
+//             "tulip",
+//             "dandelion"
+//         ]
+//     },
+//     {
+//         type: "trees",
+//         list: [
+//             "fir",
+//             "pine",
+//             "birch"
+//         ]
+//     }
+// ];
+// const secondTree = myPlants[1].list[1];
+
+const recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name']
     },
-    {
-        type: "trees",
-        list: [
-            "fir",
-            "pine",
-            "birch"
-        ]
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
     }
-];
-const secondTree = myPlants[1].list[1];
+};
+function updateRecords(records, id, prop, value) {
+    if (value === "") {
+        delete records[id][prop];
+    } else if (prop !== "tracks" && value !== "") {
+        records[id][prop] = value;
+    } else if (prop === "tracks" && value !== "") {
+        if (records[id].hasOwnProperty("tracks") === false) {
+            records[id][prop] = [];
+        }
+        records[id][prop].push(value);
+    }
+    return records;
+}
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
