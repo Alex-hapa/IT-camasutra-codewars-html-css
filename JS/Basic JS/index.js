1a253e9cc7605c1aa9bf1f1569c6e149165683f9
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-07-11 21:03:11 +0300

    feat: add next solution task

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -657,15 +657,35 @@ Hello JS
 //     }
 // ];
 
-const myStorage = {
-    "car": {
-        "inside": {
-            "glove box": "maps",
-            "passenger seat": "crumbs"
-        },
-        "outside": {
-            "trunk": "jack"
-        }
+// const myStorage = {
+//     "car": {
+//         "inside": {
+//             "glove box": "maps",
+//             "passenger seat": "crumbs"
+//         },
+//         "outside": {
+//             "trunk": "jack"
+//         }
+//     }
+// };
+// const gloveBoxContents = myStorage.car.inside['glove box'];
+
+const myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
     }
-};
-const gloveBoxContents = myStorage.car.inside['glove box'];
+];
+const secondTree = myPlants[1].list[1];
