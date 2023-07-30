b10d3b83ecdd9e0e4c0358ef72e8cce25104282b
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-07-30 19:38:52 +0300

    feat: add next solution task

--- JS/Basic JS/index.js
+++ JS/Basic JS/index.js
@@ -836,8 +836,14 @@ Hello JS
 //
 // convertToInteger("56");
 
-function convertToInteger(str) {
-    return parseInt(str, 2)
+// function convertToInteger(str) {
+//     return parseInt(str, 2)
+// }
+//
+// convertToInteger("10011");
+
+function checkEqual(a, b) {
+    return a === b ? 'Equal' : 'Not Equal';
 }
 
-convertToInteger("10011");
+checkEqual(1, 2);
